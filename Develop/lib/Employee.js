7fa92d8dad7a678b3f8331e2c5a4741473753c0a
Ben Loney <benloney@gmail.com>
2020-04-01 18:23:38 -0500

    updated employee

--- Develop/lib/Employee.js
+++ Develop/lib/Employee.js
@@ -1,1 +1,30 @@
 // TODO: Write code to define and export the Employee class
+function getEmployee() {
+    class Employee {
+        constructor(name, id, title, email) {
+            this.name = name;
+            this.id = id;
+            this.title = title;
+            this.email = email;
+        }
+        getName() {
+            console.log(name)
+            return this.name;
+        }
+        getId() {
+            console.log(id)
+            return this.id;
+        }
+
+        getEmail() {
+            console.log(this.email);
+            return this.email;
+        }
+        getRole() {
+            console.log(this.title);
+            return 'Employee';
+        }
+    }
+}
+
+module.exports = getEmployee;
