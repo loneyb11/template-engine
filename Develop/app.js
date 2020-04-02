908fc32ca9a4e5d3a2545d86e536457043988984
Ben Loney <benloney@gmail.com>
2020-04-02 00:48:14 -0500

    update

--- Develop/app.js
+++ Develop/app.js
@@ -1,42 +1,199 @@
+const inquirer = require("inquirer");
 const Manager = require("./lib/Manager");
 const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
-const jest = require('jest');
-const inquirer = require("inquirer");
-const path = require("path");
-const fs = require("fs");
-​
-const OUTPUT_DIR = path.resolve(__dirname, "output")
-const outputPath = path.join(OUTPUT_DIR, "team.html");
-​
-const render = require("./lib/htmlRenderer");
-​
-const employees = [];
+const create = require("./lib/create");
+const managerArray = [];
+const engineerArray = [];
+const internArray = [];
 
-const questions [
-  
-}
-];
+promptManager = function() {
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "name",
+        message: "What is your managers first name?",
+        validate: function(val) {
+          return /^[a-zA-Z]+$/i.test(val) || "Must only be letters!";
+        }
+      },
+      {
+        type: "input",
+        name: "idnumber",
+        message: "What is your managers id ?",
+        validate: function(val) {
+          return (
+            /^[0-9]*$/i.test(val) || "Must be a number/numbers between 0-9"
+          );
+        }
+      },
+      {
+        type: "input",
+        name: "email",
+        message: "What is your managers Email?",
+        validate: function(val) {
+          return (
+            /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/i.test(
+              val
+            ) || "Must be an email address like john@aol.com!"
+          );
+        }
+      },
+      {
+        type: "input",
+        name: "officeNumber",
+        message: "What is your managers office phone number?",
+        validate: function(val) {
+          return (
+            /^[2-9]\d{2}-\d{3}-\d{4}$/i.test(val) ||
+            "Must be a phone number in this format 123-456-7890!"
+          );
+        }
+      }
+    ])
+    .then(managerAnswers => {
+      const man = new Manager(
+        managerAnswers.name,
+        managerAnswers.idnumber,
+        managerAnswers.email,
+        managerAnswers.officeNumber
+      );
+      managerArray.push(man);
+      askForEmployee();
+    });
+};
 
-// Write code to use inquirer to gather information about the development team members,
-// and to create objects for each team member (using the correct classes as blueprints!)
-​
-// After the user has input all employees desired, call the `render` function (required
-// above) and pass in an array containing all employee objects; the `render` function will
-// generate and return a block of HTML including templated divs for each employee!
-​
-// After you have your html, you're now ready to create an HTML file using the HTML
-// returned from the `render` function. Now write it to a file named `team.html` in the
-// `output` folder. You can use the variable `outputPath` above target this location.
-// Hint: you may need to check if the `output` folder exists and create it if it
-// does not.
-​
-// HINT: each employee type (manager, engineer, or intern) has slightly different
-// information; write your code to ask different questions via inquirer depending on
-// employee type.
-​
-// HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
-// and Intern classes should all extend from a class named Employee; see the directions
-// for further information. Be sure to test out each class and verify it generates an 
-// object with the correct structure and methods. This structure will be crucial in order
-// for the provided `render` function to work!```
+askForEmployee = function() {
+  inquirer
+    .prompt([
+      {
+        type: "list",
+        name: "selection",
+        message: "What type of team member would you like to add?",
+        choices: [
+          "Engineer",
+          "Intern",
+          "I don't want to add any more team members"
+        ]
+      }
+    ])
+    .then(val => {
+      if (val.selection === "Engineer") {
+        engineerQuestions();
+      } else if (val.selection === "Intern") {
+        internQuestions();
+      } else if (
+        val.selection === "I don't want to add any more team members"
+      ) {
+        create(managerArray, internArray, engineerArray);
+      }
+    });
+};
+
+internQuestions = function() {
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "name",
+        message: "What is your intern's  first name?",
+        validate: function(val) {
+          return /^[a-zA-Z]+$/i.test(val) || "Must only be letters!";
+        }
+      },
+      {
+        type: "input",
+        name: "idnumber",
+        message: "What is your intern's ID?",
+        validate: function(val) {
+          return (
+            /^[0-9]*$/i.test(val) || "Must be a number/numbers between 0-9"
+          );
+        }
+      },
+      {
+        type: "input",
+        name: "email",
+        message: "What is your intern's email?",
+        validate: function(val) {
+          return (
+            /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/i.test(
+              val
+            ) || "Must be an email address like john@aol.com!"
+          );
+        }
+      },
+      {
+        type: "input",
+        name: "school",
+        message: "What is your intern's school?",
+        validate: function(val) {
+          return /[a-z1-9]/i.test(val) || "Must only be letters!";
+        }
+      }
+    ])
+    .then(internAnswers => {
+      const int = new Intern(
+        internAnswers.name,
+        internAnswers.idnumber,
+        internAnswers.email,
+        internAnswers.school
+      );
+      internArray.push(int);
+      askForEmployee();
+    });
+};
+
+engineerQuestions = function() {
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "name",
+        message: "What is your engineer's first name?",
+        validate: function(val) {
+          return /^[a-zA-Z]+$/i.test(val) || "Must only be letters!";
+        }
+      },
+      {
+        type: "input",
+        name: "idnumber",
+        message: "What is your engineers ID?",
+        validate: function(val) {
+          return (
+            /^[0-9]*$/i.test(val) || "Must be a number/numbers between 0-9"
+          );
+        }
+      },
+      {
+        type: "input",
+        name: "email",
+        message: "What is your email?",
+        validate: function(val) {
+          return /^\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$/i.test(val);
+        }
+      },
+      {
+        type: "input",
+        name: "github",
+        message: "What is your engineers Github?",
+        validate: function(val) {
+          return /^(\w+\S+)$/i.test(val) || "Must be a Github user name!";
+        }
+      }
+    ])
+    .then(engineerAnswers => {
+      const eng = new Engineer(
+        engineerAnswers.name,
+        engineerAnswers.idnumber,
+        engineerAnswers.email,
+        engineerAnswers.github
+      );
+      engineerArray.push(eng);
+
+      askForEmployee();
+    });
+};
+
+promptManager();
